6b81c1d14788ed6af3b4f2724fbcb1e3a5fa5225
= <=>
2019-04-22 00:08:56 +0100

    finishing and testing algorithm for generating reciept numbers to be identified

--- src/CustomAlgorithm.java
+++ src/CustomAlgorithm.java
@@ -1,20 +1,10 @@
 import java.sql.Time;
 
-public class customAlgorithm {
-    String vehicleRegistration;
-    VehicleType vehicleType;
-    Time startTime;
-    int parkingZone;
-    int parkingSpace;
-
+public class CustomAlgorithm {
     private final long max_length = 1000000000;
+    private final long min_length = 100000000;
 
-    public customAlgorithm() {
-        this.vehicleRegistration = null;
-        this.vehicleType = null;
-        this.startTime = null;
-        this.parkingZone = 0;
-        this.parkingSpace = 0;
+    public CustomAlgorithm() {
     }
 
     long generateReceiptNumber(String vehicleRegistration, VehicleType vehicleType, Time startTime, int parkingZone, int parkingSpace){
@@ -37,134 +27,133 @@ public class customAlgorithm {
                 multiplier = 1400000;
                 break;
         }
-        result = (result * (multiplier + parkingSpace + parkingSpace));
+        result = (result * (multiplier + parkingSpace + parkingZone));
 
 
         //we want consistent 9 digit receipt number so let's clamp it....
+        while(result < min_length){
+            result = result * 10;
+        }
+
         while(result > max_length){
             result = result / 10;
         }
-
-        if(result < 99999999){
-
-        }
-
-
         return result;
     }
 
     int convertStringToInt(String str){
         int result = 0;
-        for(int i = 0; i <= str.length(); i++){
-            str.toLowerCase(); //saves on switch statements...
+        str.toLowerCase(); //saves switch statements...
+        str.replace(" ", ""); //remove whitespaces...
+        for(int i = 0; i < str.length(); i++){
             switch(str.charAt(i)) {
-                case 0:
-                    result += 0;
+                case '0':
+                    result += (i* 0);
                     break;
-                case 1:
-                    result += 1;
+                case '1':
+                    result += (i* 1);
                     break;
-                case 2:
-                    result += 2;
+                case '2':
+                    result += (i* 2);
                     break;
-                case 3:
-                    result += 3;
+                case '3':
+                    result += (i* 3);
                     break;
-                case 4:
-                    result += 4;
+                case '4':
+                    result += (i* 4);
                     break;
-                case 5:
-                    result += 5;
+                case '5':
+                    result += (i* 5);
                     break;
-                case 6:
-                    result += 6;
+                case '6':
+                    result += (i* 6);
                     break;
-                case 7:
-                    result += 7;
+                case '7':
+                    result += (i* 7);
                     break;
-                case 8:
-                    result += 8;
+                case '8':
+                    result += (i* 8);
                     break;
-                case 9:
-                    result += 9;
+                case '9':
+                    result += (i* 9);
                     break;
                 case 'a':
-                    result += 10;
+                    result += (i* 10);
                     break;
                 case 'b':
-                    result += 11;
+                    result += (i* 11);
                     break;
                 case 'c':
-                    result += 12;
+                    result += (i* 12);
                     break;
                 case 'd':
-                    result += 13;
+                    result += (i* 13);
                     break;
                 case 'e':
-                    result += 14;
+                    result += (i* 14);
                     break;
                 case 'f':
-                    result += 15;
+                    result += (i* 15);
                     break;
                 case 'g':
-                    result += 16;
+                    result += (i* 16);
                     break;
                 case 'h':
-                    result += 17;
+                    result += (i* 17);
                     break;
                 case 'i':
-                    result += 18;
+                    result += (i* 18);
                     break;
                 case 'j':
-                    result += 19;
+                    result += (i* 19);
                     break;
                 case 'k':
-                    result += 20;
+                    result += (i* 20);
                     break;
                 case 'l':
-                    result += 21;
+                    result += (i* 21);
                     break;
                 case 'm':
-                    result += 22;
+                    result += (i* 22);
                     break;
                 case 'n':
-                    result += 23;
+                    result += (i* 23);
                     break;
                 case 'o':
-                    result += 24;
+                    result += (i* 24);
                     break;
                 case 'p':
-                    result += 25;
+                    result += (i* 25);
                     break;
                 case 'q':
-                    result += 26;
+                    result += (i* 26);
                     break;
                 case 'r':
-                    result += 27;
+                    result += (i* 27);
                     break;
                 case 's':
-                    result += 28;
+                    result += (i* 28);
                     break;
                 case 't':
-                    result += 29;
+                    result += (i* 29);
                     break;
                 case 'u':
-                    result += 30;
+                    result += (i* 30);
                     break;
                 case 'v':
-                    result += 31;
+                    result += (i* 31);
                     break;
                 case 'w':
-                    result += 32;
+                    result += (i* 32);
                     break;
                 case 'x':
-                    result += 33;
+                    result += (i* 33);
                     break;
                 case 'y':
-                    result += 34;
+                    result += (i* 34);
                     break;
                 case 'z':
-                    result += 35;
+                    result += (i* 35);
                     break;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.sql.Time;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        CarPark carPark = new CarPark();
-
+       // CarPark carPark = new CarPark();
+        CustomAlgorithm test = new CustomAlgorithm();
+        System.out.println(test.generateReceiptNumber("0X65 AWD",VehicleType.STANDARD, Time.valueOf(LocalTime.now()), 5,19));
+        System.out.println(test.generateReceiptNumber("2222 RXP",VehicleType.STANDARD, Time.valueOf(LocalTime.now()), 5,18));
     }
 }
